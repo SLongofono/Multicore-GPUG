6ca52a5be64641af84881d21867d85888e0840e3
Stephen <onofognol@ku.edu>
2018-03-24 13:47:54 -0500

    Skeleton working, still have weird alignment issues

--- Project_2/project2/main.cpp
+++ Project_2/project2/main.cpp
@@ -4,29 +4,68 @@
 #include <cassert>
 #include <cmath>
 #include "ImageReader.h"
+#include <cstdio>
+#include <memory>
+#include <fstream>
+#define DIMS_MSG_TAG 0
+#define DATA_MSG_TAG 1
 #define DEBUG 1
+
+using namespace std;
 
 // For testing only
 
 #if DEBUG
-#include <fstream>
-#include <cstdio>
-#include <memory>
+#define COUNT 0
 #endif
 
 #define TEST4 1
 
-using namespace std;
 
 template<typename ... Args>
 string string_format( const std::string& format, Args ... args )
 {
     size_t size = snprintf( nullptr, 0, format.c_str(), args ... ) + 1; // Extra space for '\0'
-    unique_ptr<char[]> buf( new char[ size ] ); 
+    unique_ptr<char[]> buf( new char[ size ] );
     snprintf( buf.get(), size, format.c_str(), args ... );
     return string( buf.get(), buf.get() + size - 1 ); // We don't want the '\0' inside
 }
 
+
+// dumps packed counts
+void dump(string fname, float *data){
+	ofstream chan(fname);
+	if(chan.good()){
+		for(int i = 0; i<256*3; i+=3){
+			chan << i/3 << ": (" << data[i] << "," << data[i+1] << "," << data[i+2] << ")" << endl;
+		}
+		chan.close();
+	}
+}
+
+
+
+// dumps packed counts
+void dump(int rank, int *data, int color){
+	ofstream chan;
+	if(0 == color){
+		chan.open(string_format("node_%d_red.txt", rank));
+	}
+	else if (1 == color){
+		chan.open(string_format("node_%d_green.txt", rank));
+	}
+	else{
+		chan.open(string_format("node_%d_blue.txt", rank));
+	}
+	if(chan.good()){
+		for(int i = 0; i<256; ++i){
+			chan << i << ":" << data[i] << endl;
+		}
+		chan.close();
+	}
+}
+
+// dumps channels
 void dump(int rank, float *channels){
 	ofstream rchan(string_format("node_%d_red.txt", rank));
 	ofstream gchan(string_format("node_%d_green.txt", rank));
@@ -76,31 +115,75 @@ void stats(int rank, float *channels){
 
 }
 
-void get_red_count(int *&dest, unsigned char *data, int dim1, int dim2){
-	dest = new int[256]();
-	for(int i = 0; i<dim1; ++i){
-		for(int j = 0; j<dim2; ++j){
-			dest[data[i*dim2*3 + j*3]]++;
-		}
-	}
-}
+/*
+int * count(int *data, int dim1, int dim2, int dim3){
+	int *ret = new int[256]();
 
-void get_green_count(int *&dest, unsigned char *data, int dim1, int dim2){
-	dest = new int[256]();
-	for(int i = 0; i<dim1; ++i){
-		for(int j = 0; j<dim2; ++j){
-			dest[data[i*dim2*3 + j*3 + 1]]++;
-		}
+	for(int i = 0; i<256; ++i){
+		assert(ret[i] == 0);
 	}
-}
 
-void get_blue_count(int *&dest, unsigned char *data, int dim1, int dim2){
-	dest = new int[256]();
+	int arg;
 	for(int i = 0; i<dim1; ++i){
 		for(int j = 0; j<dim2; ++j){
-			dest[data[i*dim2*3 + j*3 + 2]]++;
+			arg = i*dim2*3 + j*3 + dim3;
+			ret[data[arg]]++;
 		}
 	}
+	return ret;
+}	
+*/
+
+float * histogram(int *data, int dim1, int dim2, int rank){
+	float* ret = new float[256*3];
+	int arg;
+	int red[256] = {0};
+	int green[256] = {0};
+	int blue[256] = {0};
+	for(int i = 0; i<dim1; ++i){
+		for(int j = 0; j<dim2; ++j){
+			arg = i*dim2*3 + j*3;
+			red[ data[arg] ]++;
+			green[ data[arg+1] ]++;
+			blue[ data[arg+2] ]++;
+		}
+	}
+
+
+	//red = count(data, dim1, dim2, 0);
+	//green = count(data, dim1, dim2, 1);
+	//blue = count(data, dim1, dim2, 2);
+#if DEBUG
+	dump(rank, red, 0);
+	dump(rank, green, 1);
+	dump(rank, blue, 2);
+#endif
+	float norm = 1.0/(dim1*dim2);
+
+	arg = 0;
+	// Normalize and assign
+	for(int j = 0; j<256; ++j){
+		ret[arg] = norm * red[j];
+		ret[arg + 1] = norm * green[j];
+		ret[arg + 2] = norm * blue[j];
+		arg += 3;
+	}
+	
+
+	/*	
+	for(int i=0, j=0; i<256; ++i, j +=3){
+		ret[j] = red[i]*norm;
+		ret[j+1] = green[i]*norm;
+		ret[j+2] = blue[i]*norm;
+	}
+	*/
+
+	//delete [] red;
+	//delete [] green;
+	//delete [] blue;
+
+	return ret;
+	
 }
 
 /*
@@ -110,10 +193,11 @@ void get_blue_count(int *&dest, unsigned char *data, int dim1, int dim2){
  * packed in triplets (r1,g1,b1,r2,g2,b3...).  Note we pass by reference since
  * heap allocation will change the address used in virtual memory
  */
-void normalized_count(float *&dest, unsigned char *data, int dim1, int dim2){
+/*
+void normalized_count(float *&dest, int *data, int dim1, int dim2){
 	dest = new float[256*3];
 	int arg;
-	float norm = dim1 * dim2;
+	float norm = 1.0/(dim1 * dim2);
 	
 	// Accumulate pixel value counts
 	int rcounts[256] = {0};
@@ -129,18 +213,16 @@ void normalized_count(float *&dest, unsigned char *data, int dim1, int dim2){
 		}
 	}
 
-	cout << endl;
-
 	// Assign normalized percentage to each pixel value, in packed order
 	// in destination array
 	for(int i = 0, j=0; i < 256; ++i, j = j + 3){
-		dest[j] = rcounts[i] / norm;
-		dest[j+1] = gcounts[i] / norm;
-		dest[j+2] = bcounts[i] / norm;
+		dest[j] = rcounts[i] * norm;
+		dest[j+1] = gcounts[i] * norm;
+		dest[j+2] = bcounts[i] * norm;
 	}
 
 }
-
+*/
 
 /*
  * Compares the 256x(r,g,b) packed histogram in myHisto to the numNodes other
@@ -158,6 +240,7 @@ int compare_histos(float *myHisto, float *otherHistos, int myRank, int numNodes)
 
 	// For each rank in the world...
 	for(int n = 0; n < numNodes; ++n){
+		currDiff = 0.0;
 		if(n != myRank){
 			// Since they are all packed the same way, compare
 			// elementwise
@@ -169,7 +252,6 @@ int compare_histos(float *myHisto, float *otherHistos, int myRank, int numNodes)
 				minDiff = currDiff;
 			}
 			cout << "NODE " << myRank << ": difference between me and node " << n << " is " << currDiff << endl;
-			currDiff = 0.0;
 		}
 	}
 	return lowRank;
@@ -191,26 +273,43 @@ int main(int argc, char **argv){
 		// We are not the root
 
 		// Wait for dimensions information in a tag 0 message
-		MPI_Recv(dims, 2, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+		MPI_Recv(dims, 2, MPI_INT, 0, DIMS_MSG_TAG, MPI_COMM_WORLD, &status);
 		cout << "NODE " << rank << ": dimensions: (" << dims[0] << "," << dims[1] << ")" << endl;
 			
-		unsigned char rawData[dims[0]*dims[1]*3];
+		int rawData[dims[0]*dims[1]*3];
+
+		cout << "NODE " << rank << ": catching data of size " << dims[0]*dims[1]*3*sizeof(int) << endl;
+		cout << "NODE " << rank << ": size of destination in bytes: " << sizeof(rawData) << endl;
+
 
 		// Wait for image data in a tag 1 message
-		MPI_Recv(rawData, sizeof(rawData), MPI_UNSIGNED_CHAR, 0, 1, MPI_COMM_WORLD, &status);
+		MPI_Recv(rawData, dims[0]*dims[1]*3, MPI_INT, 0, DATA_MSG_TAG, MPI_COMM_WORLD, &status);
 
 		cout << "NODE " << rank << ": got image with " << dims[0]*dims[1] << " pixels..." << endl;
 
 
 		// Compute histograms
-		float *channels;
-		normalized_count(channels, rawData, dims[0], dims[1]);
+		float *channels = histogram(rawData, dims[0], dims[1], rank);
 
+#if DEBUG
+		dump(string_format("NODE_%d_CHANNELS.txt", rank), channels);
+#endif
+		//normalized_count(channels, rawData, dims[0], dims[1]);
 
 		// All-to-all gather to get results from other ranks
+		// Use blocking since we need everyone to be done before we
+		// start to compare
 		// Declare a buffer big enough for everything
 		float histos[256*3*numNodes];
-		MPI_Allgather(channels, 256*3, MPI_FLOAT, histos, 256*3, MPI_FLOAT, MPI_COMM_WORLD);
+		
+		// fill in my data
+		for(int i = 0; i<256*3; ++i){
+			histos[256*3*rank + i] = channels[i];
+		}
+		
+		MPI_Allgather(MPI_IN_PLACE, 0, MPI_DATATYPE_NULL, histos, 256*3, MPI_FLOAT, MPI_COMM_WORLD);
+
+		// Without in place MPI_Allgather(channels, 256*3, MPI_FLOAT, histos, 256*3, MPI_FLOAT, MPI_COMM_WORLD);
 
 #if DEBUG
 		// Sanity check.  My data should start at 256*3*myRank
@@ -219,27 +318,23 @@ int main(int argc, char **argv){
 			assert(channels[k] == histos[offset + k]);
 		}
 
-		stats(rank, channels);
-		dump(rank, channels);
-		//cout << "NODE " << rank << ": " << 100*channels[512 + 253] << " percent of my pixels are max red" << endl;
-		//cout << "NODE " << rank << ": " << 100*channels[512 + 254] << " percent of my pixels are max green" << endl;
-		//cout << "NODE " << rank << ": " << 100*channels[512 + 255] << " percent of my pixels are max blue" << endl;
 #endif
 
 		// Determine the most similar rank, and report back to rank 0
 		
-		int closest;
-		closest = compare_histos(channels, histos, rank, numNodes);
+		int closest = compare_histos(channels, histos, rank, numNodes);
 		cout << "NODE " << rank << ": most similar to NODE " << closest << endl;
+
+		// Clean up
+		delete [] channels;
 
 
 	}
 	else{
 		// We are the root
-		cout << "ROOT " << rank << endl;
 
 		for(int n = 1; n < numNodes; ++n){
-			cout << argv[n+1] << endl;
+			cout << "ROOT: sending " << argv[n+1] << " to rank " << n << endl;
 
 			// Prepare a packed array from the image
 			ImageReader *ir = ImageReader::create(argv[n+1]);
@@ -251,22 +346,38 @@ int main(int argc, char **argv){
 			// Send a message indicating the dimensions
 			dims[0] = arr->getDim1();
 			dims[1] = arr->getDim2();
-			MPI_Isend(dims, 2, MPI_INT, n, 0, MPI_COMM_WORLD, &rq);
+			MPI_Isend(dims, 2, MPI_INT, n, DIMS_MSG_TAG, MPI_COMM_WORLD, &rq);
 
 			// Flatten the data for transit
-			unsigned char flatpack[dims[0]*dims[1]*3];
+			int flatpack[dims[0]*dims[1]*3];
 			for(int i = 0; i<dims[0]; ++i){
 				for(int j =0; j<dims[1]; ++j){
 					for(int k = 0; k<3; ++k){
 						// 3D indexing: i*cols*size + j*size + k
-						size_t arg = (i*dims[1]*3) + (j*3) + k;
-						flatpack[arg] = arr->getDataElement(i,j,k);
+						int arg = (i*dims[1]*3) + (j*3) + k;
+						flatpack[arg] = (int)arr->getDataElement(i,j,k);
 					}
 				}
 			}
 
-			// Fire it off to the appropriate node
-			MPI_Isend(flatpack, dims[0]*dims[1]*3, MPI_UNSIGNED_CHAR, n, 1, MPI_COMM_WORLD, &rq);
+/*
+#if DEBUG
+			int *red, *green, *blue;
+			red = count(flatpack, dims[0], dims[1], 0);
+			green = count(flatpack, dims[0], dims[1], 1);
+			blue = count(flatpack, dims[0], dims[1], 2);
+			dump(100+n, red, 0);
+			dump(100+n, green, 1);
+			dump(100+n, blue, 2);
+			delete []red;
+			delete []green;
+			delete []blue;
+#endif
+*/
+
+			// Fire it off to the appropriate node, no need to
+			// wait around to make sure it got there
+			MPI_Isend(flatpack, dims[0]*dims[1]*3, MPI_INT, n, DATA_MSG_TAG, MPI_COMM_WORLD, &rq);
 		}
 
 		// Do our work while the rest complete
@@ -276,38 +387,51 @@ int main(int argc, char **argv){
 		arr = ir->getInternalPacked3DArrayImage();
 		dims[0] = arr->getDim1();
 		dims[1] = arr->getDim2();
-		unsigned char flatpack[dims[0]*dims[1]*3];
+		int flatpack[dims[0]*dims[1]*3];
 		for(int i = 0; i<dims[0]; ++i){
 			for(int j =0; j<dims[1]; ++j){
 				for(int k = 0; k<3; ++k){
 				// 3D indexing: i*cols*size + j*size + k
-					size_t arg = (i*dims[1]*3) + (j*3) + k;
-					flatpack[arg] = arr->getDataElement(i,j,k);
+					int arg = (i*dims[1]*3) + (j*3) + k;
+					flatpack[arg] = (int)arr->getDataElement(i,j,k);
 				}
 			}
 		}
 
 
 		// Compute our histogram
-		float *channels;
-		normalized_count(channels, flatpack, dims[0], dims[1]);	
-
+		float *channels = histogram(flatpack, dims[0], dims[1], 0);
+		//normalized_count(channels, flatpack, dims[0], dims[1]);	
 #if DEBUG
-		stats(0, channels);
-		dump(0, channels);
+		dump(string_format("NODE_%d_CHANNELS.txt", 0), channels);
 #endif
 		// All-to-all gather to get histograms from other ranks
 		// Declare a buffer big enough for everything
 		float histos[256*3*numNodes];
-		MPI_Allgather(channels, 256*3, MPI_FLOAT, histos, 256*3, MPI_FLOAT, MPI_COMM_WORLD);
+
+		// Copy my data into the appropriate area (since local work is
+		// less expensive than communication)
+		for(int i = 0; i<256*3; ++i){
+			histos[i] = channels[i];
+		}
+
+		MPI_Allgather(MPI_IN_PLACE, 0, MPI_DATATYPE_NULL, histos, 256*3, MPI_FLOAT, MPI_COMM_WORLD);
+		//   Without in place: MPI_Allgather(channels, 256*3, MPI_FLOAT, histos, 256*3, MPI_FLOAT, MPI_COMM_WORLD);
+
+		// Sanity check: my data should be at rank*256*3
+		for(int i = 0; i<256*3; ++i){
+			assert(channels[i] == histos[i + rank*256*3]);
+		}
 
 		// Determine the most similar image and report
-		int closest;
-		closest = compare_histos(channels, histos, rank, numNodes);
+		int closest = compare_histos(channels, histos, rank, numNodes);
 		cout << "NODE " << rank << ": most similar to NODE " << closest << endl;
 		
 		// Catch and report results from each other rank
 		
+
+		// Clean up
+		delete [] channels;
 
 	}
 
