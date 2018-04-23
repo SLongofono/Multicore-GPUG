cbb2cf9b8015e99e8d5bac047bd7ceaed538645b
Stephen <onofognol@ku.edu>
2018-04-22 23:59:08 -0500

    Some planning and helper functions

---
+++ Project_3/proj3/kernels.h
@@ -0,0 +1,15 @@
+/*
+ * Kernel functions for project 3
+ */
+
+__global__ void kernel_max(int nCols, int nRows, int depth, unsigned char *data, unsigned char *results){
+	
+}
+
+__global__ void kernel_sum_1(int nCols, int nRows, int depth, unsigned char *data, float *maxes){
+	
+}
+
+__global__ void kernel_sum_2(int nCols, int nRows, int depth, unsigned char *data, float *maxes, unsigned char * results){
+	
+}
